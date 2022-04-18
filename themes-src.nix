59fd052635fe571e2dff3d19717a1301b823a912
Jakub Darul <daruljakub@protonmail.com>
2022-04-18 11:20:47 +0200

    fix: spicetify-themes repo name

--- themes-src.nix
+++ themes-src.nix
@@ -2,7 +2,7 @@ let
   pkgs = import <nixpkgs> {};
 in
   pkgs.fetchFromGitHub {
-    owner = "morpheusthewhite";
+    owner = "spicetify";
     repo = "spicetify-themes";
     rev = "0d36944b848fa19715bda0d7364cfad472faee9f";
     sha256 = "r6MD6Z4eDyblTXy0fFkv48/7Z2Y8IvJt12jriXPk1yY=";
--- update.sh
+++ update.sh
@@ -2,4 +2,4 @@
 #!nix-shell -i bash -p nix-prefetch-github
 
 rm themes-src.nix
-nix-prefetch-github --nix --rev master morpheusthewhite spicetify-themes > themes-src.nix
+nix-prefetch-github --nix --rev master spicetify spicetify-themes > themes-src.nix
